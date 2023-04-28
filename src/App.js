0e3a0d98fe2403ce350743023074dc018ef47813
Karishma-tejwani <karishmatejwani9@gmail.com>
2023-04-28 22:19:07 +0500

    index

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <h1>Hello World</h1>
+      <p style={{ fontFamily: "Alkatra" }}>
+        Google Fonts is the leading host of open-source fonts for use on the
+        web, with hundreds of font families available for inclusion in web pages
+        or download. Learn ...
+      </p>
     </>
   );
 }
