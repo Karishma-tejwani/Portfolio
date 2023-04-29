4d8dad9104974ad6d8c7c28d4134dd456b6746e1
Karishma-tejwani <karishmatejwani9@gmail.com>
2023-04-29 23:14:31 +0500

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import linkedin from "../../assets/linkedin.png";
 import github from "../../assets/github.png";
 import logo from "../../assets/logo.png";
 import { GoThreeBars } from "react-icons/go";
+import { MdOutlineClose } from "react-icons/md";
 import "../Navbar/navbar.css";
 
 const Navbar = () => {
+  const [isShow, setShow] = useState(false);
+
   return (
     <nav>
       <div className="container nav_container">
         <Link to={"/"} className="logo">
           <img src={logo} alt="logo" />
         </Link>
-        <ul className="navLinks">
+        <ul className={`navLinks ${isShow ? "show_nav" : "hide_nav"}`}>
           <li>
-            <NavLink to={"/"}>Home</NavLink>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) => (isActive ? "active-nav" : "")}
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to={"/about"}>About</NavLink>
+            <NavLink
+              to={"/about"}
+              className={({ isActive }) => (isActive ? "active-nav" : "")}
+            >
+              About
+            </NavLink>
           </li>
           <li>
-            <NavLink to={"/skills"}>Skills</NavLink>
+            <NavLink
+              to={"/skills"}
+              className={({ isActive }) => (isActive ? "active-nav" : "")}
+            >
+              Skills
+            </NavLink>
           </li>
           <li>
-            <NavLink to={"/experience"}>Experience</NavLink>
+            <NavLink
+              to={"/experience"}
+              className={({ isActive }) => (isActive ? "active-nav" : "")}
+            >
+              Experience
+            </NavLink>
           </li>
           <li>
-            <NavLink to={"/projects"}>Projects</NavLink>
+            <NavLink
+              to={"/projects"}
+              className={({ isActive }) => (isActive ? "active-nav" : "")}
+            >
+              Projects
+            </NavLink>
           </li>
           <li>
-            <NavLink to={"/contact"}>Contact</NavLink>
+            <NavLink
+              to={"/contact"}
+              className={({ isActive }) => (isActive ? "active-nav" : "")}
+            >
+              Contact
+            </NavLink>
           </li>
           {/* <span className="navbar-text">
             <div className="social-icon">
@@ -46,8 +79,8 @@ const Navbar = () => {
             </div>
           </span> */}
         </ul>
-        <button className="nav_toggle_btn">
-          <GoThreeBars />
+        <button className="nav_toggle_btn" onClick={() => setShow(!isShow)}>
+          {isShow ? <MdOutlineClose /> : <GoThreeBars />}
         </button>
       </div>
     </nav>
