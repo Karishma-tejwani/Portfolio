9295f37c1988e69c86212d4d523a93b833af7433
Karishma-tejwani <karishmatejwani9@gmail.com>
2023-04-30 00:55:30 +0500

    Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,30 @@
 import React from "react";
-import Header from "./Header";
+import { Link } from "react-router-dom";
 import "../Home/home.css";
+import profile from "../../assets/profile.png";
 
 function Home() {
   return (
-    <>
-      <Header />
-    </>
+    <header className="home">
+      <div className="container home_container">
+        <div className="home-left">
+          <h1>FrontEnd Developer</h1>
+          <p>
+            lorem loremloremloremloremloremloremloremloremlorem
+            loremloremloremloremloremloremloremloremloremloremloremloremlorem
+          </p>
+          <Link to={"/contact"} className="btn">
+            Let's Connect
+          </Link>
+        </div>
+        <div className="home-right">
+          <div className="home-circle"></div>
+          <div className="home-img">
+            <img src={profile} alt="profile" />
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
 
