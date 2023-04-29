da07d36319140143a18a0a225a4002eed3900c42
Karishma-tejwani <karishmatejwani9@gmail.com>
2023-04-29 21:45:28 +0500

    index

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Experience from "./components/Experience/Experience";
+import Skills from "./components/Skills/Skills";
+import Projects from "./components/Projects/Projects";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
-      <p style={{ fontFamily: "Alkatra" }}>
-        Google Fonts is the leading host of open-source fonts for use on the
-        web, with hundreds of font families available for inclusion in web pages
-        or download. Learn ...
-      </p>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
