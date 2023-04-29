eba29193bdb910724e034e48c6e39ea73d55d56c
Karishma-tejwani <karishmatejwani9@gmail.com>
2023-04-29 22:27:14 +0500

    index

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import linkedin from "../../assets/linkedin.png";
+import github from "../../assets/github.png";
+import logo from "../../assets/logo.png";
+import { GoThreeBars } from "react-icons/go";
+import "../Navbar/navbar.css";
 
-function Navbar() {
-  return <h1>Hello world</h1>;
-}
-
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="container nav_container">
+        <Link to={"/"} className="logo">
+          <img src={logo} alt="logo" />
+        </Link>
+        <ul className="navLinks">
+          <li>
+            <NavLink to={"/"}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/about"}>About</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/skills"}>Skills</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/experience"}>Experience</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/projects"}>Projects</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/contact"}>Contact</NavLink>
+          </li>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a
+                href="https://www.linkedin.com/in/karishma-tejwani-5424a2153/"
+                target="_blank"
+              >
+                <img src={linkedin} alt="linkedin" />
+              </a>
+              <a href="https://github.com/Karishma-tejwani" target="_blank">
+                <img src={github} alt="github" />
+              </a>
+            </div>
+          </span>
+        </ul>
+        <button className="nav_toggle_btn">
+          <GoThreeBars />
+        </button>
+      </div>
+    </nav>
+  );
+};
 export default Navbar;
