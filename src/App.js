0668fac419c3bdccd8c6c09365f7583aceae9aba
Karishma-tejwani <karishmatejwani9@gmail.com>
2023-05-07 16:01:09 +0500

    App

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Contact from "./components/Contact/Contact";
 import Experience from "./components/Experience/Experience";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
-import PageNotFound from "./components/PageNotFound/PageNotFound";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
@@ -40,7 +39,6 @@ function App() {
       <Experience />
       <Projects />
       <Contact /> */}
-      {/* <PageNotFound /> */}
       {/* <Footer /> */}
     </>
   );
