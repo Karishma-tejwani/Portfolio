396c34356af9bca8b6304c097c18de7afa534883
Karishma-tejwani <karishmatejwani9@gmail.com>
2023-05-07 15:50:40 +0500

    Contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,8 @@
-import contact from "../../assets/contact.jpg";
-import { FaEnvelope } from "react-icons/fa";
 import React, { useRef } from "react";
 import emailjs from "emailjs-com";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { FaEnvelope } from "react-icons/fa";
+import { MdLocationOn, MdLibraryAddCheck } from "react-icons/md";
 import "../Contact/contact.css";
 
 const Contact = () => {
@@ -22,47 +23,75 @@ const Contact = () => {
     <section className="contact">
       <h1 className="text-center">Get In Touch</h1>
       <div className="contact-container my-5">
-        <div className="contact-opts">
-          <img
-            src={contact}
-            alt="contact"
-            className="rounded mx-5 contact-img"
-          />
-          <p className="contact-text">
-            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used
-            in laying out print, graphic or web designs. The passage is
-            attributed to an unknown Lorem ipsum, or lipsum as it is sometimes
-            known
-          </p>
-          <article className="contact-opt">
-            <span>
-              <FaEnvelope />
-              karishmatejwani9@gmail.com
-            </span>
-            <br />
-            <a href="mailto:karishmatejwani9@gmail.com" target="_blank">
-              Send a Message
-            </a>
-          </article>
+        <div className="contact-info" data-aos="fade-up">
+          <div className="contact-info-details">
+            <ul>
+              <li>
+                <MdLocationOn size={"23px"} />
+                Sukkur Sindh, Pakistan
+              </li>
+
+              <li>
+                <FaEnvelope size={"20px"} />
+                <a href="mailto:karishmatejwani9@gmail.com" target="_blank">
+                  karishmatejwani9@gmail.com
+                </a>{" "}
+              </li>
+
+              <li>
+                <MdLibraryAddCheck size={"20px"} />
+                Frontend Developer
+              </li>
+            </ul>
+          </div>
+          <div className="contact__social">
+            <p className="contact__linkedin">
+              <a
+                href="https://www.linkedin.com/in/karishma-tejwani-5424a2153/"
+                target="_blank"
+              >
+                <BsLinkedin size={30} />
+              </a>
+            </p>
+
+            <p className="contact__github">
+              <a href="https://github.com/Karishma-tejwani" target="_blank">
+                <BsGithub size={30} />
+              </a>
+            </p>
+          </div>
         </div>
 
         {/* contact form */}
-        <form ref={form} onSubmit={sendEmail}>
-          <input
-            type="text"
-            name="name"
-            placeholder="Enter Fullname"
-            required
-          />
-          <input type="email" name="email" placeholder="Enter Email" required />
-          <input
-            type="text"
-            name="phone"
-            placeholder="Enter phone number"
-            required
-          />
-          <textarea name="message" rows={7} placeholder="Message" required />
-          <button className="btn btn-primary contact-btn">
+        <form ref={form} onSubmit={sendEmail} data-aos="fade-up">
+          <p className="contact__form-group">
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter FullName"
+              required
+            />
+          </p>
+          <p className="contact__form-group">
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter Email"
+              required
+            />
+          </p>
+          <p className="contact__form-group">
+            <input
+              type="text"
+              name="phone"
+              placeholder="Enter phone Number"
+              required
+            />
+          </p>
+          <p className="contact__form-group">
+            <textarea name="message" rows={7} placeholder="Message" required />
+          </p>
+          <button type="submit" className="contact-btn">
             Send a message
           </button>
         </form>
