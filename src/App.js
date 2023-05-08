eb0b7ef353c715e84acf91035190b37a404cf903
Karishma-tejwani <karishmatejwani9@gmail.com>
2023-05-08 23:55:58 +0500

    App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Experience from "./components/Experience/Experience";
@@ -12,26 +11,6 @@ import Home from "./components/Home/Home";
 function App() {
   return (
     <>
-      {/* <BrowserRouter>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <>
-                <Navbar />
-                <Home />
-                <About />
-                <Skills />
-                <Experience />
-                <Projects />
-                <Contact />
-                <Footer />
-              </>
-            }
-          />
-        </Routes>
-      </BrowserRouter> */}
       <Navbar />
       <Home />
       <About />
